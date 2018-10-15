dca71cae96d5dd2240268d10713d3f7b7dc3b774
Unknown <gman10501@gmail.com>
2018-10-14 18:46:08 -0700

    Currently Fixing Input Manager Problem
    Button Input seems to be shared among all controllers

---
+++ Assets/Scripts/Menus/PlayerSelectScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelectScreen : MonoBehaviour
+{
+    public PlayerOrder playerOrder;
+    public bool isReady = false;
+
+    public bool IsReady { get { return isReady; } }
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SelectionInput();
+
+    }
+
+    private void SelectionInput()
+    {
+        if (ControllerManager.Instance.GetAButtonDown(playerOrder))
+        {
+            isReady = true;
+        }
+        else if (ControllerManager.Instance.GetBButtonDown(playerOrder))
+        {
+            isReady = false;
+        }
+    }
+}
