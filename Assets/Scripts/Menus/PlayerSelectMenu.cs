bdef82815ae7e3b5d934448aba68ef408e04d89f
Unknown <gman10501@gmail.com>
2018-10-20 01:54:06 -0700

    Initial Multiplayer Testing Complete, Player Data saving and loading complete

--- Assets/Scripts/Menus/PlayerSelectMenu.cs
+++ Assets/Scripts/Menus/PlayerSelectMenu.cs
@@ -4,68 +4,85 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
+/*******************************************************************************************************/
+// This class is used to control the UI in the MainMenu scene
+/*******************************************************************************************************/
 public class PlayerSelectMenu : MonoBehaviour
 {
-    public GameObject[] playerPanels;
-    public Text[] controllerConnected;
-    public Text[] controllerDisconnected;
-    public Text[] playerReady;
-    public Image startGame;
+    public GameObject[] playerPanels;       // The sections for each player to select characters and join game
+    public Text[] controllerConnected;      // User message to show if controller is connected
+    public Text[] controllerDisconnected;   // User message to show if controller is disconnected
+    public Text[] playerReady;              // User message to show if controller is connected and player is ready
+    public Image startGame;                 // An image that only appears if at least one player is ready
 
+    /*******************************************************************************************************/
     // Use this for initialization
+    /*******************************************************************************************************/
     void Start()
     {
 
     }
+    /*******************************************************************************************************/
 
+    /*******************************************************************************************************/
     // Update is called once per frame
+    /*******************************************************************************************************/
     void Update()
     {
-        FindPlayers();
+        FindPlayers();  // Used to find player and check connectivity of controllers
 
     }
+    /*******************************************************************************************************/
 
+    /*******************************************************************************************************/
+    // Used to find player and check connectivity of controllers
+    /*******************************************************************************************************/
     private void FindPlayers()
     {
-        for (int count = 0; count < ControllerManager.Instance.controllers.Length; count++)
+        for (int count = 0; count < ControllerManager.Instance.controllers.Length; count++)     // Used to check through each controller connected
         {
-            if (ControllerManager.Instance.controllers[count] == null || ControllerManager.Instance.controllers[count] == "")
+            if (ControllerManager.Instance.controllers[count] == null || ControllerManager.Instance.controllers[count] == "")   // Checks to see if controller is connected or disconnected
             {
-                playerReady[count].gameObject.SetActive(false);
-                controllerConnected[count].gameObject.SetActive(false);
-                controllerDisconnected[count].gameObject.SetActive(true);
+                playerReady[count].gameObject.SetActive(false);             // Sets player ready message to off
+                controllerConnected[count].gameObject.SetActive(false);     // Sets controller connected message to off
+                controllerDisconnected[count].gameObject.SetActive(true);   // Sets controller disconnected message to on
             }
             else
             {
-                JoinGame(count);
-                controllerDisconnected[count].gameObject.SetActive(false);
+                JoinGame(count);                                                // Used to set the player as ready to play
+                controllerDisconnected[count].gameObject.SetActive(false);      // Sets controller disconnected message to off
             }
         }
 
         if (playerPanels[0].GetComponent<PlayerSelectScreen>().IsReady || playerPanels[1].GetComponent<PlayerSelectScreen>().IsReady ||
-            playerPanels[2].GetComponent<PlayerSelectScreen>().IsReady || playerPanels[3].GetComponent<PlayerSelectScreen>().IsReady)
+            playerPanels[2].GetComponent<PlayerSelectScreen>().IsReady || playerPanels[3].GetComponent<PlayerSelectScreen>().IsReady)       // Checks to see if at least one player is ready
         {
-            startGame.gameObject.SetActive(true);
-            EventSystem.current.SetSelectedGameObject(startGame.gameObject);
+            startGame.gameObject.SetActive(true);                               // Sets start game image to on
         }
         else
         {
-            startGame.gameObject.SetActive(false);
+            startGame.gameObject.SetActive(false);                              // Sets start game image to off
         }
     }
+    /*******************************************************************************************************/
 
+    /*******************************************************************************************************/
+    // Used to set the player as ready to play
+    /*******************************************************************************************************/
     private void JoinGame(int count)
     {
-        if (playerPanels[count].GetComponent<PlayerSelectScreen>().IsReady)
+        if (playerPanels[count].GetComponent<PlayerSelectScreen>().IsReady)     // Checks to see if the player is ready or not
         {
-            playerReady[count].gameObject.SetActive(true);
-            controllerConnected[count].gameObject.SetActive(false);
-            
+            playerReady[count].gameObject.SetActive(true);                      // Sets player ready message to on
+            controllerConnected[count].gameObject.SetActive(false);             // Sets controller connected message to off
+
         }
         else
         {
-            playerReady[count].gameObject.SetActive(false);
-            controllerConnected[count].gameObject.SetActive(true);
+            playerReady[count].gameObject.SetActive(false);                     // Sets player ready message to off
+            controllerConnected[count].gameObject.SetActive(true);              // Sets controller connected message to on
         }
     }
+    /*******************************************************************************************************/
 }
+/*******************************************************************************************************/
--- Assets/Scripts/Menus/PlayerSelectScreen.cs
+++ Assets/Scripts/Menus/PlayerSelectScreen.cs
@@ -2,53 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*******************************************************************************************************/
+// This class is used to set the player data into the game
+/*******************************************************************************************************/
 public class PlayerSelectScreen : MonoBehaviour
 {
-    public PlayerOrder playerOrder;
-    public string characterName;
-    private bool isReady = false;
+    public PlayerOrder playerOrder;     // Used to set which player is which in the inspector
+    public string characterName;        // Used to set the name of the selected character into the game
+    private bool isReady = false;       // Used to check if the player has selected which character and is set to play
 
-    public bool IsReady { get { return isReady; } }
+    public bool IsReady { get { return isReady; } }     // This property is used to check in the PlayerSelectMenu if the player has selected which character and is set to play
 
-	// Use this for initialization
-	void Start ()
+    /*******************************************************************************************************/
+    // Use this for initialization
+    /*******************************************************************************************************/
+    void Start ()
     {
 		
 	}
-	
-	// Update is called once per frame
-	void Update ()
+    /*******************************************************************************************************/
+
+    /*******************************************************************************************************/
+    // Update is called once per frame
+    /*******************************************************************************************************/
+    void Update ()
     {
-        SelectionInput();
+        SelectionInput();   // Used to make selections for the player or to go back
 
     }
+    /*******************************************************************************************************/
 
+    /*******************************************************************************************************/
+    // Used to make selections for the player or to go back
+    /*******************************************************************************************************/
     private void SelectionInput()
     {
-        if (ControllerManager.Instance.GetAButtonDown(playerOrder))
+        if (ControllerManager.Instance.GetAButtonDown(playerOrder))     // Checks if A button is pressed on X-Input controller
         {
-            if (GameManager.Instance.playerQuantity < ControllerManager.Instance.controllers.Length && !isReady)
+            if (GameManager.Instance.playerQuantity < ControllerManager.Instance.controllers.Length && !isReady)    // Checks if the player quantity is less than the number of controllers connected
             {
-                GameManager.Instance.playerQuantity++;
-                isReady = true;
+                SetPlayerData();    // Used to set the player data after the player is ready to play
+
+                GameManager.Instance.playerQuantity++;  // Increase player quantity by one when player is ready
+                isReady = true;                         // Player is ready to play
             }
         }
-        else if (ControllerManager.Instance.GetBButtonDown(playerOrder))
+        else if (ControllerManager.Instance.GetBButtonDown(playerOrder))    // Checks if B button is pressed on X-Input controller
         {
-            if (GameManager.Instance.playerQuantity > 0 && isReady)
+            if (GameManager.Instance.playerQuantity > 0 && isReady)         // Checls if player quantity is greater than 0 and if the player is ready or not
             {
-                GameManager.Instance.playerQuantity--;
-                isReady = false;
+                GameManager.Instance.DeletePlayerData(playerOrder);         // Deletes existing player data that was already set after being ready
+
+                GameManager.Instance.playerQuantity--;                      // Decreases player quantity by one when player goes back
+                isReady = false;                                            // Player is not ready to play
             }
         }
 
-        if (isReady)
+        if (isReady)    // Checks to see if player is ready
         {
-            if (ControllerManager.Instance.GetStartButtonDown(playerOrder))
+            if (ControllerManager.Instance.GetStartButtonDown(playerOrder))     // Checks to see if Start button is pressed on X-Input controller
             {
-                GameManager.Instance.SavePlayerData(playerOrder, characterName);
-                GameManager.Instance.ChangeScene("MultiplayerTest");
+                GameManager.Instance.ChangeScene("MultiplayerTest");            // Changes to the scene where the game begins
             }
         }
     }
+    /*******************************************************************************************************/
+
+    /*******************************************************************************************************/
+    // Used to set the player data after the player is ready to play
+    /*******************************************************************************************************/
+    private void SetPlayerData()
+    {
+        switch (playerOrder)    // Checks to see which player is this
+        {
+            case PlayerOrder.PLAYER_1:                                                  
+                GameManager.Instance.SavePlayerData(playerOrder, characterName);    // Saves player 1 data
+                //Debug.Log("Saved Player 1 Data");
+                break;
+            case PlayerOrder.PLAYER_2:
+                GameManager.Instance.SavePlayerData(playerOrder, characterName);    // Saves player 2 data
+                break;
+            case PlayerOrder.PLAYER_3:
+                GameManager.Instance.SavePlayerData(playerOrder, characterName);    // Saves player 3 data
+                break;
+            case PlayerOrder.PLAYER_4:
+                GameManager.Instance.SavePlayerData(playerOrder, characterName);    // Saves player 4 data
+                break;
+        }
+    }
+    /*******************************************************************************************************/
 }
+/*******************************************************************************************************/
