a7b5b068a6d9eaba78b327713e5a7165d9de177b
Diego Quinonez <quinonezdiego@yahoo.com>
2018-11-27 22:43:12 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/PickUps/RandomDrop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDrop : MonoBehaviour {
+
+    public GameObject[] dropitems;
+    float droprate = 0.25f;
+
+    public void DropItem()
+    {
+        if (Random.Range(0f, 1f) <= droprate)
+        {
+            int indexToDrop = Random.Range(0, dropitems.Length);
+            Instantiate(dropitems[indexToDrop], this.transform.position, this.transform.rotation);
+        }
+    }
+}
